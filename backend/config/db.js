4efa7c052c3652fb68bdd7695d7a56cc16ffe371
vishalprajapati15 <vishalprajapati3266@gmail.com>
2026-01-11 10:30:08 +0530

    error fixed

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,17 @@
 import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 export const connectDB = async ()=>{
-    await mongoose.connect('mongodb+srv://kartiksingh0737_db_user:QzqfJsCAXoS7qlHR@cluster0.uhqp1o4.mongodb.net/Invoice')
-    .then(()=>{
-        console.log("DB Connected Successfully !!");
-    })
+    try {
+        await mongoose.connect(process.env.MONGODB_URI,{
+            dbName:process.env.DB_NAME
+        })
+        .then(()=>{
+            console.log("DB Connected Successfully !!");
+        });
+    } catch (error) {
+        console.log("DB Connection failed !!");
+    }
 }
