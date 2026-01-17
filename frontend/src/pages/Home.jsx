1d3e78eddab660646a8d94a2fdba5b790e1620cd
vishalprajapati15 <vishalprajapati3266@gmail.com>
2026-01-18 01:33:45 +0530

    features section complete

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Navbar from '../components/Navbar.jsx'
+import Hero from './Hero.jsx'
+import Features from '../components/Features.jsx'
 
 const Home = () => {
   return (
     <div>
-        <Navbar />
+      <Navbar />
+      <main className=''>
+      <Hero/>
+      <div className=''>
+        <Features />
+      </div>
+      </main>
     </div>
   )
 }
